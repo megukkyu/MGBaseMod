9042089adbe6d1029c91b1ce2e9ca523757d611e
megukkyu <megukkyu@excite.co.jp>
2018-02-20 03:55:51 +0900

    add RegisterItemModels

---
+++ src/main/java/org/mgqclub/mgbasemod/client/RegisterItemModels.java
@@ -0,0 +1,24 @@
+package org.mgqclub.mgbasemod.client;
+
+import org.mgqclub.mgbasemod.MGBaseMod;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+
+public class RegisterItemModels {
+
+	public static void registerItemModels(ModelRegistryEvent event) {
+		registerNormalItemModels(event);
+	}
+	
+	private static void registerNormalItemModels(ModelRegistryEvent event) {
+		registerNormalItemModel(event,MGBaseMod.ITEMS.item_copper_ingot);
+	}
+	
+	private static void registerNormalItemModel(ModelRegistryEvent event, Item item) {
+		ModelLoader.setCustomModelResourceLocation(item,0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+	}
+	
+}
