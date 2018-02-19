c895444fc7e6936513973c8d372a66bbbe5e7b14
megukkyu <megukkyu@excite.co.jp>
2018-02-20 00:28:57 +0900

    first setting 1

---
+++ src/main/java/org/mgqclub/mgbasemod/MGBaseMod.java
@@ -0,0 +1,27 @@
+package org.mgqclub.mgbasemod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = MGBaseMod.MODID, name = MGBaseMod.NAME, version = MGBaseMod.VERSION)
+public class MGBaseMod {
+    public static final String MODID = "mgbasemod";
+    public static final String NAME = "MGBaseMod";
+    public static final String VERSION = "0.1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    	
+    }
+}
