ff2241712cd45099c53aec504ae6ea308718ae65
megukkyu <megukkyu@excite.co.jp>
2018-02-20 03:48:40 +0900

    fix RegisterItems.java

--- src/main/java/org/mgqclub/mgbasemod/item/RegisterItems.java
+++ src/main/java/org/mgqclub/mgbasemod/item/RegisterItems.java
@@ -1,8 +1,5 @@
 package org.mgqclub.mgbasemod.item;
 
-import org.mgqclub.mgbasemod.MGBaseMod;
-import org.mgqclub.mgbasemod.Reference;
-
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
@@ -14,13 +11,15 @@ public class RegisterItems {
 	}
 	
 	private static void registerNormalItems(RegistryEvent.Register<Item> event) {
-		registerNormalItem(event, MGBaseMod.ITEMS.item_copper_ingot, "item_copper_ingot");
+		registerNormalItem(event, "item_copper_ingot");
 	}
 	
-	private static void registerNormalItem(RegistryEvent.Register<Item> event, Item item, String name) {
-		item.setRegistryName(Reference.MODID, name);
-		item.setCreativeTab(CreativeTabs.MATERIALS);
-		item.setUnlocalizedName(name);
-		event.getRegistry().register(item);
+	private static void registerNormalItem(RegistryEvent.Register<Item> event, String name) {
+		event.getRegistry().register(
+				new Item()
+				.setRegistryName(name)
+				.setCreativeTab(CreativeTabs.MATERIALS)
+				.setUnlocalizedName(name)
+				);
 	}
 }
