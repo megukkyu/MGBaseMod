dbbbd67adf2db9f6e34c59ad506a0037cf6991b8
megukkyu <megukkyu@excite.co.jp>
2018-02-20 18:41:08 +0900

    Version up to alpha 0.1.2

--- src/main/java/org/mgqclub/mgbasemod/Reference.java
+++ src/main/java/org/mgqclub/mgbasemod/Reference.java
@@ -3,7 +3,7 @@ package org.mgqclub.mgbasemod;
 public class Reference {
     public static final String MODID = "mgbasemod";
     public static final String NAME = "MGBaseMod";
-    public static final String VERSION = "0.1.1";
+    public static final String VERSION = "0.1.2";
     public static final String CLIENT_PROXY = "org.mgqclub.mgbasemod.proxy.ClientProxy";
     public static final String SERVER_PROXY = "org.mgqclub.mgbasemod.proxy.ServerProxy";
     
