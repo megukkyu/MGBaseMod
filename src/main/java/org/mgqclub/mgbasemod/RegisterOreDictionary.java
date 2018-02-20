4523884ac9ad07cb13b1da496754c5c8564f693b
Alpha2198 <akiyo03@hotmail.co.jp>
2018-02-20 21:41:19 +0900

    Add silver to OreDict

--- src/main/java/org/mgqclub/mgbasemod/RegisterOreDictionary.java
+++ src/main/java/org/mgqclub/mgbasemod/RegisterOreDictionary.java
@@ -8,5 +8,7 @@ public class RegisterOreDictionary {
 	public static void registerOres() {
 		OreDictionary.registerOre("oreCopper",new ItemStack(MGBaseMod.BLOCKS.block_copper_ore));
 		OreDictionary.registerOre("ingotCopper", new ItemStack(MGBaseMod.ITEMS.item_copper_ingot));
+		OreDictionary.registerOre("oreSilver",new ItemStack(MGBaseMod.BLOCKS.block_silver_ore));
+		OreDictionary.registerOre("ingotSilver", new ItemStack(MGBaseMod.ITEMS.item_silver_ingot));
 	}
 }
